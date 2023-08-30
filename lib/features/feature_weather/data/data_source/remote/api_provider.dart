8521b2461f4f4578dcb8047f6a51d2d73594b52a
mohammad-zakeri <mohammadzakeri322@gmail.com>
2023-08-30 21:50:02 +0430

    implementation model and entity

--- lib/features/feature_weather/data/data_source/remote/api_provider.dart
+++ lib/features/feature_weather/data/data_source/remote/api_provider.dart
@@ -1,7 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:weather_app/core/utils/constants.dart';
 
 class ApiProvider{
 
   final Dio _dio = Dio();
+  var apiKey = Constants.apiKey;
+
+  Future<dynamic> callCurrentWeather({required String cityName}) async{
+
+    var response = await _dio.get(
+
+      '${Constants.baseUrl}/data/2.5/weather',
+
+      queryParameters: {
+        'q' : cityName,
+        'appid' : apiKey,
+        'units' : 'metric'
+      }
+
+    );
+
+    return response;
+
+  }
 
 }
