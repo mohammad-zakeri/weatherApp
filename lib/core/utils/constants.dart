8baddbd154ec0d1c09fbbb16b6e8dda12b4dfc60
mohammad-zakeri <mohammadzakeri322@gmail.com>
2023-09-01 19:57:44 +0430

    repository pattern

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,4 +2,7 @@
 class Constants{
   static const baseUrl = "https://openweathermap.org";
   static const apiKey = "ba03fd0ec924a544eb2887e983e51419";
+
+  static const error = "SomeThing went Wrong. try again...";
+  static const internetError = "please check your connection...";
 }
