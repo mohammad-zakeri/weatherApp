38ef0cc4e1021ea8b445799d29603a8e74f9cc63
mohammad-zakeri <mohammadzakeri322@gmail.com>
2023-09-10 22:48:54 +0430

    bloc

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/features/feature_weather/presentation/bloc/current_weather_status.dart';
+import 'package:weather_app/features/feature_weather/presentation/bloc/home_bloc.dart';
 
-class MainWrapper extends StatelessWidget {
+class MainWrapper extends StatefulWidget {
   const MainWrapper({super.key});
+
+  @override
+  State<MainWrapper> createState() => _MainWrapperState();
+}
+
+class _MainWrapperState extends State<MainWrapper> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<HomeBloc>(context).add(LoadCurrentWeatherEvent("Tehran"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +26,27 @@ class MainWrapper extends StatelessWidget {
 
       appBar: AppBar(),
 
-      body: Container(),
+      body: BlocBuilder<HomeBloc, HomeState>(
+
+        builder: (context, state) {
+
+          if(state.currentWeatherStatus is CurrentWeatherLoading){
+            return const Center(child: Text('loading ...'));
+          }
+
+          if(state.currentWeatherStatus is CurrentWeatherCompleted){
+            return const Center(child: Text('completed'));
+          }
+
+          if(state.currentWeatherStatus is CurrentWeatherError){
+            return const Center(child: Text('error'));
+          }
+
+          return Container();
+
+        },
+
+      ),
 
     );
 
